f63b3a50e6a0c868f50172af0c1f78e58b9c31e8
Tracy Ngo <tngo1206@gmail.com>
2019-02-10 02:10:45 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(collection)
+  i = 0
+  updated_collection = []
+  while i < collection.length
+  updated_collection.push(yield (collection[i]))
+  i+= 1
+  end
+  updated_collection
+end
 
