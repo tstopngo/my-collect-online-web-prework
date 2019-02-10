8297cd3435339f89cc616befdef6763b87be1f88
Tracy Ngo <tngo1206@gmail.com>
2019-02-10 02:10:26 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0
   updated_collection = []
   while i < collection.length
-  yield (collection[i])
+  updated_collection.push(yield (collection[i]))
   i+= 1
   end
   collection
